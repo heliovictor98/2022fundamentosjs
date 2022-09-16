3a8ee7771467aa588092fe3ff6899fedcd39014a
Helio Barros <barros.helio98@gmail.com>
2022-09-15 21:32:54 -0300

    exemplos de variaves e tipos

---
+++ type-number.js
@@ -0,0 +1,48 @@
+// tipo Number
+
+// const meuNumero = 3;
+
+const primeiroNumero = 6;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+const operacaoMatematica2 = primeiroNumero - segundoNumero;
+const operacaoMatematica3 = primeiroNumero * segundoNumero;
+const operacaoMatematica4 = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+console.log(operacaoMatematica2)
+console.log(operacaoMatematica3)
+console.log(operacaoMatematica4)
+
+
+// // ponto flutuante
+
+// const numeroPontoFlutuante = 3.3;
+// const numeroFlutuanteSemZero = .5;
+
+// const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+// console.log(novaOperacao);
+
+// // NaN -> Not A Number (Não é um número)
+
+//  const alura = "Alura";
+
+//  console.log(alura * primeiroNumero);
+
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura123!";
+const StringDeNumeros = "123456";
+
+const citacao = "Hélio V";
+const meuNome = "Victor";
+const convertMinusculo = meuNome.toLowerCase();
+
+
+console.log(citacao + meuNome)
+
+console.log(citacao === convertMinusculo)
+
+console.log(citacao.length)
